31e5019f7597b385dddc54b62f36c7b70e5b6bd2
Cannur Kartum <48458722+CannurKartum@users.noreply.github.com>
2024-08-30 00:37:49 +0200

    add ui fragments and xml files

---
+++ app/src/main/java/com/example/kekodproject/ui/kindnessFragment.kt
@@ -0,0 +1,30 @@
+package com.example.kekodproject.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kekodproject.databinding.FragmentKindnessBinding
+
+class kindnessFragment : Fragment() {
+
+    private var _binding: FragmentKindnessBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View{
+        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment
+        _binding = FragmentKindnessBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
